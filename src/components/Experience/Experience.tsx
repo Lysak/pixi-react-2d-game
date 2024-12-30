90314bcdb681fc1af656da73d478a89a3bf7975b
Roman1510 <romanvinnick@gmail.com>
2024-12-30 23:22:00 +0100

    changing the folders

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useCallback } from 'react'
 import { Stage } from '@pixi/react'
-import { MainContainer } from './MainContainer'
+import { MainContainer } from './MainContainer/MainContainer'
 import { calculateCanvasSize } from '../../helpers/common'
 
 export const Experience = () => {
